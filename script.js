e03df068492617c2e773cd10d2ab3f0bd4816fd8
gronfagelab <gronfagelab@gmail.com>
2024-06-23 13:39:36 +0200

    adding new files

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = navLink.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});
